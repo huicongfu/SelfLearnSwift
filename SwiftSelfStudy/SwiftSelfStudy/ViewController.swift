7d753a50b5be95847e74a4368218eab7861e2e26
fu_Mac <fuhuicong@qq.com>
2017-10-13 07:14:53 +0800

    init

---
+++ SwiftSelfStudy/SwiftSelfStudy/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SwiftSelfStudy
+//
+//  Created by fu on 2017/10/12.
+//  Copyright © 2017年 fhc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    var tableView = UITableView
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGray;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
