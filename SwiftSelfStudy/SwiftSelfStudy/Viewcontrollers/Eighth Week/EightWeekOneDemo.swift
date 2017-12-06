b333c0f4691156a1f1b85399b5718f5b97e3cacc
fu_Mac <fuhuicong@qq.com>
2017-12-06 23:47:28 +0800

    第八周第二个demo

---
+++ SwiftSelfStudy/SwiftSelfStudy/Viewcontrollers/Eighth Week/EightWeekOneDemo.swift
@@ -0,0 +1,84 @@
+//
+//  EightWeekOneDemo.swift
+//  SwiftSelfStudy
+//
+//  Created by fu on 2017/12/6.
+//  Copyright © 2017年 fhc. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class EightWeekOneDemo: UIViewController,ARSCNViewDelegate {
+    
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // 利用SceneKit创建一个球体，并这是材质
+        let sphere = SCNSphere(radius: 0.2);
+        let material = SCNMaterial();
+        material.diffuse.contents = UIImage(named: "art.scnassets/mercury.jpg");
+        sphere.materials = [material];
+        
+        //创建节点，并设置几何体为上面的球体
+        let node = SCNNode();
+        node.position = SCNVector3(x:0, y:0.1, z:-0.5);
+        node.geometry = sphere;
+        
+        //将节点添加到scene中
+        sceneView.scene.rootNode.addChildNode(node);
+        sceneView.autoenablesDefaultLighting = true;
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+
+    // MARK: - ARSCNViewDelegate
+    
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
+     
+     return node
+     }
+     */
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+
+}
--- SwiftSelfStudy/SwiftSelfStudy/Viewcontrollers/Eighth Week/EighthWeekVC.swift
+++ SwiftSelfStudy/SwiftSelfStudy/Viewcontrollers/Eighth Week/EighthWeekVC.swift
@@ -8,14 +8,55 @@
 
 import UIKit
 
-class EighthWeekVC: UIViewController {
+private let cellId = "cellId8"
 
+class EighthWeekVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    let dataArray = ["第一个-数据库","第二个-ARKit-月球","第二个-ARKit-尺子"]
+    var tableView = UITableView.init(frame: UIScreen.main.bounds, style: UITableViewStyle.plain)
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.createUI()
     }
-
+    
+    func createUI() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.tableFooterView = UIView()
+        self.view.addSubview(tableView)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = dataArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            let vc = SevenWeekOneDemo.init()
+            self.navigationController?.pushViewController(vc, animated: true)
+        case 1:
+            let vc = EightWeekOneDemo.init(nibName: "EightWeekOneDemo", bundle: Bundle.main)
+            vc.navigationItem.title = dataArray[indexPath.row]
+            self.navigationController?.pushViewController(vc, animated: true)
+            
+        default:
+            break
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
